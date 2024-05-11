0bee9b5cc055dafa825bee25e005cb8f55df470a
ahmet <dede_dede_ahmet@hotmail.com>
2024-05-11 11:10:32 +0300

    Added a new project

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,4 +1,5 @@
-import todo from '../assets/todo-app.png'
+import todo from '../assets/projects/todo-app.png'
+import spring from '../assets/projects/java-spring.jpg'
 
 const projects = [
     {
@@ -7,6 +8,13 @@ const projects = [
         description: 'A Vue 3 todo app that leverages Pinia for state management, allowing users to easily track and update their tasks. It features a responsive and visually appealing interface designed with Tailwind CSS.',
         blitz: 'https://stackblitz.com/~/github.com/ahmedy00/vue-todo',
         github: 'https://github.com/ahmedy00/vue-todo'
+    },
+    {
+        name: 'Layered Architecture and Dependency Injection',
+        image: spring,
+        description: 'It is a repository that covers how layered architecture and dependency injection concepts are handled in Java Spring with sample codes and documents. Aims to improve personal learning and application skills',
+        blitz:'https://stackblitz.com/~/github.com/ahmedy00/spring-layered-architecture-and-dependency-injection',
+        github:'https://github.com/ahmedy00/spring-layered-architecture-and-dependency-injection',
     }
 ]
 
